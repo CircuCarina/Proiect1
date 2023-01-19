6093755fa10a1f7eeed6ec961132c7470af71da8
CircuCarina <badita.carina@yahoo.com>
2023-01-19 23:54:38 +0200

    .

---
+++ Models/ToyData.cs
@@ -0,0 +1,8 @@
+﻿namespace Proiect1.Models
+{
+    public class ToyData
+    {
+        public IEnumerable<ToyT> Toys { get; set; }
+        public IEnumerable<CategoryC> Categories { get; set; }
+    }
+}
--- Pages/Toys/Index.cshtml.cs
+++ Pages/Toys/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Proiect1.Data;
 using Proiect1.Models;
@@ -19,10 +20,15 @@ namespace Proiect1.Pages.Toys
             _context = context;
         }
 
-        public IList<ToyT> ToyT { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public IList<ToyT> ToyT { get; set; } = default!;
+        public string ToySort { get; set; }
+        public string CategorySort { get; set; }
+        public string CurrentFilter { get; set; }
+        public async Task OnGetAsync(string sortOrder)
         {
+            ToySort = String.IsNullOrEmpty(sortOrder) ? "toy_desc" : "";
+            CategorySort = String.IsNullOrEmpty(sortOrder) ? "category_desc" : "";
+
             if (_context.ToyT != null)
             {
                 ToyT = await _context.ToyT
@@ -30,6 +36,17 @@ namespace Proiect1.Pages.Toys
                     .Include(a => a.Category)
                     .Include(a => a.Brand)
                     .ToListAsync();
+            }
+            switch (sortOrder)
+            {
+                case "toy_desc":
+                    ToyData.Toys = ToyData.Toys.OrderByDescending(s =>
+                   s.Toy);
+                    break;
+                case "category_desc":
+                    ToyData.Toys = ToyData.Toys.OrderByDescending(s =>
+                   s.Category);
+                    break;
             }
         }
     }
